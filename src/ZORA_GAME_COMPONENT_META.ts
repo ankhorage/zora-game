af79f755fa7183d1dabfddcbe75cea93a460b1f7
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:49:12 +0200

    style: sort Game metadata imports

--- src/ZORA_GAME_COMPONENT_META.ts
+++ src/ZORA_GAME_COMPONENT_META.ts
@@ -1,6 +1,6 @@
-import { gameMeta } from './features/game-presentation/meta/gameMeta';
 import { gameEntityMeta } from './features/game-presentation/meta/gameEntityMeta';
 import { gameFieldMeta } from './features/game-presentation/meta/gameFieldMeta';
+import { gameMeta } from './features/game-presentation/meta/gameMeta';
 import { gameOverlayMeta } from './features/game-presentation/meta/gameOverlayMeta';
 
 /*** Register the generic game presentation metadata owned by this package. */

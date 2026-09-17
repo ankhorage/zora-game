afd4efdb013cf8373a9760d8935a38f778683490
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:54:59 +0200

    style: format Game input zone export

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,11 @@ export type {
   GamePointerEvents,
   GameProps,
 } from './features/game-presentation/public';
-export { Game, GameEntity, GameField, GameInputZone, GameOverlay } from './features/game-presentation/public';
+export {
+  Game,
+  GameEntity,
+  GameField,
+  GameInputZone,
+  GameOverlay,
+} from './features/game-presentation/public';
 export { ZORA_GAME_PLUGIN } from './ZORA_GAME_PLUGIN';

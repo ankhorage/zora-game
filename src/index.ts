4ac5776791df0fcd015b33dc3659aadca215fffb
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-17 22:32:26 +0200

    style: sort public game exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-export {
-  GameEntity,
-  GameField,
-  GameOverlay,
-  type GameEntityProps,
-  type GameFieldProps,
-  type GameOverlayPlacement,
-  type GameOverlayProps,
-  type GamePointerEvents,
+export type {
+  GameEntityProps,
+  GameFieldProps,
+  GameOverlayPlacement,
+  GameOverlayProps,
+  GamePointerEvents,
 } from './features/game-presentation/public';
+export { GameEntity, GameField, GameOverlay } from './features/game-presentation/public';
 export { ZORA_GAME_PLUGIN } from './ZORA_GAME_PLUGIN';

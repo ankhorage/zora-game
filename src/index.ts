6e2fe2b0e87f04f4ca715415d1a0752e35d46989
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:49:19 +0200

    style: sort Game public type exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export type {
   GameEntityProps,
   GameFieldProps,
-  GameProps,
   GameOverlayPlacement,
   GameOverlayProps,
   GamePointerEvents,
+  GameProps,
 } from './features/game-presentation/public';
 export { Game, GameEntity, GameField, GameOverlay } from './features/game-presentation/public';
 export { ZORA_GAME_PLUGIN } from './ZORA_GAME_PLUGIN';

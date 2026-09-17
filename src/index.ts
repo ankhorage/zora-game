9083f953fb413cfbfa5de42ac47c40a234d9378e
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:39:01 +0200

    feat: expose Game from package entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export type {
   GameEntityProps,
   GameFieldProps,
+  GameProps,
   GameOverlayPlacement,
   GameOverlayProps,
   GamePointerEvents,
 } from './features/game-presentation/public';
-export { GameEntity, GameField, GameOverlay } from './features/game-presentation/public';
+export { Game, GameEntity, GameField, GameOverlay } from './features/game-presentation/public';
 export { ZORA_GAME_PLUGIN } from './ZORA_GAME_PLUGIN';

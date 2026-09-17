7ab65223593e0ce00a08b1cd031460535955a6d3
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:39:07 +0200

    feat: register Game component metadata

--- src/ZORA_GAME_COMPONENT_META.ts
+++ src/ZORA_GAME_COMPONENT_META.ts
@@ -1,9 +1,11 @@
+import { gameMeta } from './features/game-presentation/meta/gameMeta';
 import { gameEntityMeta } from './features/game-presentation/meta/gameEntityMeta';
 import { gameFieldMeta } from './features/game-presentation/meta/gameFieldMeta';
 import { gameOverlayMeta } from './features/game-presentation/meta/gameOverlayMeta';
 
 /*** Register the generic game presentation metadata owned by this package. */
 export const ZORA_GAME_COMPONENT_META = {
+  Game: gameMeta,
   GameEntity: gameEntityMeta,
   GameField: gameFieldMeta,
   GameOverlay: gameOverlayMeta,

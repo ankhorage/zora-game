1b07520461d1d242ac693d40a6c7908551d7f0c4
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 01:38:55 +0200

    feat: export embeddable Game component

--- src/features/game-presentation/public.ts
+++ src/features/game-presentation/public.ts
@@ -1,10 +1,12 @@
 export type {
   GameEntityProps,
   GameFieldProps,
+  GameProps,
   GameOverlayPlacement,
   GameOverlayProps,
   GamePointerEvents,
 } from '../../types/gamePresentation';
+export { Game } from './adapters/inbound/Game';
 export { GameEntity } from './adapters/inbound/GameEntity';
 export { GameField } from './adapters/inbound/GameField';
 export { GameOverlay } from './adapters/inbound/GameOverlay';

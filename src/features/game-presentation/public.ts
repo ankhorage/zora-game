603d71e790d279dcd40ab6a9557bf0345708e46c
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-18 06:15:13 +0200

    feat: expose game measurement probe API

--- src/features/game-presentation/public.ts
+++ src/features/game-presentation/public.ts
@@ -3,6 +3,7 @@ export type {
   GameFieldProps,
   GameInputZoneProps,
   GameKeyboardBinding,
+  GameMeasurementProbeProps,
   GameOverlayPlacement,
   GameOverlayProps,
   GamePointerEvents,
@@ -12,4 +13,5 @@ export { Game } from './adapters/inbound/Game';
 export { GameEntity } from './adapters/inbound/GameEntity';
 export { GameField } from './adapters/inbound/GameField';
 export { GameInputZone } from './adapters/inbound/GameInputZone';
+export { GameMeasurementProbe } from './adapters/inbound/GameMeasurementProbe';
 export { GameOverlay } from './adapters/inbound/GameOverlay';
